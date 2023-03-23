e1a6ed21c13c1ed194e4ba81a19c8daa46a3f0ef
maril06 <neckerchief06@naver.com>
2023-03-23 20:22:48 +0900

    mybatis를 이용한 list조회, insert, read

--- ex02/src/main/java/org/zerock/mapper/BoardMapper.java
+++ ex02/src/main/java/org/zerock/mapper/BoardMapper.java
@@ -7,6 +7,14 @@ import org.zerock.domain.BoardVO;
 
 public interface BoardMapper {
 	
-	@Select("select * from tbl_board where bno > 0")
+//	@Select("select * from tbl_board where bno > 0")
 	public List<BoardVO> getList();
+	
+	public void insert(BoardVO board);
+	
+	
+	//id속성 값은 메서드와 일치해야 한다. 
+	public void insertSelectKey(BoardVO board);
+	
+	public BoardVO read(Long bno);
 }
--- ex02/src/test/java/org/zerock/mapper/BoardMapperTests.java
+++ ex02/src/test/java/org/zerock/mapper/BoardMapperTests.java
@@ -5,6 +5,7 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.zerock.domain.BoardVO;
 
 import lombok.Setter;
 import lombok.extern.log4j.Log4j;
@@ -17,9 +18,55 @@ public class BoardMapperTests {
 	@Setter(onMethod_ = { @Autowired})
 	private BoardMapper mapper;
 	
+//	@Test
+//	public void testGetList() {
+//		
+//		mapper.getList().forEach(board->log.info(board));
+//	}
+	
+//	@Test
+//	public void testInsert() {
+//		
+//		BoardVO board = new BoardVO();
+//		
+//		board.setTitle("테스트 작성 글");
+//		board.setContent("테스트 작성 내용");
+//		board.setWriter("테스트 작성자");
+//		
+//		mapper.insert(board);
+//		
+//		// Lombok이 만들어주는 toString()을 이용해서 bno멤버변수의 값을 알아보기 위함.
+//		// BoardVO(bno=null, title=테스트 작성 글, content=테스트 작성 내용, writer=테스트 작성자, regdate=null, updateDate=null)
+//		// bno 값이 null인것을 확인할수 있다. 결론적으로 bno(PK) 값을 확인할 수 없다.
+//		log.info(board);
+//	}
+	
+//	@Test
+//	public void testInsertSelectKey() {
+//		
+//		BoardVO board = new BoardVO();
+//		board.setTitle("테스트 작성 글(selectKey)");
+//		board.setContent("테스트 작성 내용(selectKey)");
+//		board.setWriter("테스트 작성자(selectKey)");
+//		
+//		
+//		
+//		//BoardVO(bno=22, title=테스트 작성 글(selectKey), content=테스트 작성 내용(selectKey), writer=테스트 작성자(selectKey), regdate=null, updateDate=null)
+//		// @SelectKey를 이용하는 방식은 Sql을 한번 더 실행해서 부담이 있긴하지만,
+//		// 자동으로 처리되는 PK값을 확인해야하는 상황에서 유용하게 사용됨.
+//		mapper.insertSelectKey(board);
+//		
+//		log.info(board);
+//	}
+	
 	@Test
-	public void testGetList() {
+	public void testRead() {
 		
-		mapper.getList().forEach(board->log.info(board));
+		//존재하는 게시물 번호로 테스트
+		BoardVO board = mapper.read(22L);
+		
+		
+		//.BoardVO(bno=22, title=테스트 작성 글(selectKey), content=테스트 작성 내용(selectKey), writer=테스트 작성자(selectKey), regdate=Thu Mar 23 20:14:33 JST 2023, updateDate=Thu Mar 23 20:14:33 JST 2023)
+		log.info(board);
 	}
 }
